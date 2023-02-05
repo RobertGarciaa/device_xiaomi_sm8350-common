2863f773bec8abb9373e63c2a42f62f1ee71613b
saikiran2001 <bjsaikiran@gmail.com>
2023-02-05 03:21:30 +0000

    sm8350-common: Build more media components
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- common.mk
+++ common.mk
@@ -300,6 +300,13 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media.omx@1.0-service \
+    libminijail \
+    libavservices_minijail_32 \
+    libavservices_minijail.vendor \
+    libcodec2_vndk.vendor \
+    libcodec2_hidl@1.1.vendor \
     libmm-omxcore \
     libOmxAacEnc \
     libOmxAmrEnc \
@@ -307,6 +314,11 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxG711Enc \
     libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw \
+    libstagefright_omx.vendor \
+    libmm-omxcore \
     libstagefrighthw \
     libstagefright_omx \
     libstagefright_foundation \
@@ -352,7 +364,9 @@ PRODUCT_PACKAGES += \
 # Minijail
 PRODUCT_PACKAGES += \
     libavservices_minijail \
-    libavservices_minijail.vendor
+    libavservices_minijail.vendor \
+    libcodec2_soft_common.vendor \
+    libsfplugin_ccodec_utils.vendor
 
 # Network
 PRODUCT_COPY_FILES += \
