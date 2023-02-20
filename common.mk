8066d9c8ebc83fdec2282d2caa5c62bfc9923f95
Zinadin Zidan <zidan.roking@gmail.com>
2023-02-20 23:56:00 +0800

    sm8350-common: Guard QTI thermal 2.0 service HAL
    * It doesn't work on mars, renoir, star, and venus.
    
      E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
      E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Change-Id: I56d844402e8ec6d8e0dfd145495cc19619ae7b75

--- common.mk
+++ common.mk
@@ -527,8 +527,12 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti \
     android.hardware.thermal@2.0.vendor
+
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+endif
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
