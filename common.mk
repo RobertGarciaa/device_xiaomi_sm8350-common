63124db70dd48570ac6bc89f3b82ee9e94372792
RobertGarciaa <chae0218@naver.com>
2023-04-03 13:54:53 +0900

    sm8350-common: Remove support for Performance Mode
    This is a combination of the following:
    
    Revert "sm8350-common: sepolicy: Adress Perf hal denial"
    
    This reverts commit 9918b4ff008540a6e3928abb479f93deebc20b9c.
    
    Revert "sm8350-common: sepolicy: Adress Perf hal denial"
    
    This reverts commit b62a6d3be2544a867e3088203d4a318577dcb72c.
    
    Revert "sm8350-common: Add proper support for Pasrmanager"
    
    This reverts commit ab0cfb6aba7f90d3eb2984510852cbf7cad10dc6.
    
    Revert "sm8350-common: Add servicetracker libs"
    
    This reverts commit afa896fd8c4be9b1b11f11c6af387fee28eaf9b7.
    
    Revert "sm8350-common: Add qspmhal support"
    
    This reverts commit 27ff567201d46252c5a14edc62e54226a0a17f69.
    
    Revert "sm8350-common: Add Perf System blobs from LA.QSSI.13.0.r1-06400-qssi.0"
    
    This reverts commit 4d262140374837b17dd0acc1dad0c44dacbe09e9.
    
    Revert "sm8350-common: Update Perf stack to LA.UM.9.14.r1-20200-LAHAINA.QSSI13.0"
    
    This reverts commit 231df39a5f4452ce3b60b33416971d95d8d893b5.
    
    Revert "sm8350-common: Add IOP HAL Service"
    
    This reverts commit 6238cfdb4c3a8992dd320056af5df30f74ab158e.

--- common.mk
+++ common.mk
@@ -426,18 +426,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.3.vendor \
-    libpsi.vendor \
-    libtflite
+    vendor.qti.hardware.perf@2.3.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
-
-# Perf Jars
-PRODUCT_BOOT_JARS += \
-    QPerformance \
-    UxPerformance
 
 # Power
 PRODUCT_PACKAGES += \
