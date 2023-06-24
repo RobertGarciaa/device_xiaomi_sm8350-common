d89998a7ae6c0afaba7ecec3a6c68d0b6bfba91b
RobertGarciaa <chae0218@naver.com>
2023-06-24 18:42:58 +0900

    sm8350-common: Use AOSP Dialer
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- common.mk
+++ common.mk
@@ -195,6 +195,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += Settings
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.systemuicompilerfilter=speed
 
+# Dialer
+PRODUCT_PACKAGES += \
+    Dialer
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
