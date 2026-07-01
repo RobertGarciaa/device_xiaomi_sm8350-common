6a6535f5c87e9cadea9fd54c5980ef57334c3871
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-01 01:35:32 -0400

    sm8350-common: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -163,6 +163,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2025-11-01
 
 # Sepolicy
+include device/lineage/sepolicy/libion/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 
